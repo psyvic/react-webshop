44a6270128655f759cf3f9f771fdd0a55b8f8bf0
psyvic <victorh34@hotmail.com>
2022-01-03 11:53:47 +0100

    added screen to imports

--- frontend/__tests__/Product.test.js
+++ frontend/__tests__/Product.test.js
@@ -1,5 +1,5 @@
 import { MockedProvider } from '@apollo/react-testing';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Product from '../components/Product';
 import { fakeItem } from '../lib/testUtils';
 
